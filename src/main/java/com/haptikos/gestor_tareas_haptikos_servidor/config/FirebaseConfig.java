5e73f7960b4f70fd53b6fc93513496eaf2eb7cf8
manuel-romo <manuel.romo253080@potros.itson.edu.mx>
2026-05-19 10:38:06 -0700

    Gestor_Tareas_Haptikos_Servidor

--- src/main/java/com/haptikos/gestor_tareas_haptikos_servidor/config/FirebaseConfig.java
+++ src/main/java/com/haptikos/gestor_tareas_haptikos_servidor/config/FirebaseConfig.java
@@ -3,20 +3,27 @@ package com.haptikos.gestor_tareas_haptikos_servidor.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${FIREBASE_KEY}")
+    private String firebaseKey;
+
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/serviceAccountKey.json");
+            ByteArrayInputStream serviceAccount = new ByteArrayInputStream(
+                    firebaseKey.getBytes(StandardCharsets.UTF_8)
+            );
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
